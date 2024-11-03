3d18fd24f5c5a5ba5b166e795b726534aee6256a
Nguyen Trong Quy <trongquy131204@gmail.com>
2024-11-03 19:10:56 +0700

    Thirty Six Commit - FE
    added Javascript for admin orders

---
+++ asset/js/js_admin_orders.js
@@ -0,0 +1,26 @@
+// Accept Order button, when clicked change the innerText to On Going
+function acceptOrder(button) {
+  const orderCard = button.closest(".order-card");
+  const statusElement = orderCard.querySelector(".order-card-status");
+  statusElement.innerText = "On Going";
+  showOrdersByStatus("New");
+}
+
+function showOrdersByStatus(status) {
+  // Get all order cards
+  const orders = document.querySelectorAll(".order-card");
+
+  // Loop through each order card
+  orders.forEach((order) => {
+    const statusElement = order.querySelector(".order-card-status");
+    if (statusElement.innerText === status) {
+      order.style.display = "block";
+    } else {
+      order.style.display = "none";
+    }
+  });
+}
+
+window.onload = function () {
+  showOrdersByStatus("New");
+};
